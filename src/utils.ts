7d5f3029a4993663da79640da9492ab40a29143f
sjy <stseo012@gmail.com>
2022-06-19 20:47:17 +0900

    [feat]: 사용자가 잘못된 값을 입력한 경우 alert으로 메세지 보여주는 기능

--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,32 @@ export const createRandomNumber = () => {
 
   return Array.from(randomNumber).join("");
 };
+export const validationInputValue = (value: string) => {
+  const valueArr = Array.from(value);
+  const set = new Set(valueArr);
+
+  if (valueArr[0] === "-") {
+    alert(VALIDATION_MESSAGE.inputPositive);
+    inputInitialization();
+    return false;
+  }
+
+  if (valueArr.length !== NUMBER_LENGTH) {
+    alert(VALIDATION_MESSAGE.inputThree);
+    inputInitialization();
+    return false;
+  }
+
+  if (valueArr.length !== set.size) {
+    alert(VALIDATION_MESSAGE.inputNoRepetition);
+    inputInitialization();
+    return false;
+  }
+
+  if (valueArr.includes("0")) {
+    alert(VALIDATION_MESSAGE.inputZeroNotIncluded);
+    inputInitialization();
+    return false;
+  }
+  return true;
+};
