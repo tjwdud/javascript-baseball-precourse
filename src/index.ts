aa541a6ab057e211cdb52d20e9b99e5c53231cc4
sjy <stseo012@gmail.com>
2022-06-20 21:27:32 +0900

    [chore]: 랜덤 숫자 생성 방식 변경 & let const로 변경

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import { setRandomNum } from "./utils";
 
 export const setNumber = () => {
+  const randomNumber = randomNum();
   const btn = document.getElementById("submit");
+  const inputNumber = document.getElementById("user-input") as HTMLInputElement;
 
-  let inputNumber = document.getElementById("user-input") as HTMLInputElement;
 
   btn.addEventListener("click", (e) => {
     e.preventDefault();
     if (validationInputValue(inputNumber.value)) {
       let baseball = new (BaseballGame as any)();
 
-      baseball.play(setRandomNum("getType"), inputNumber.value);
+      baseball.play(
+        randomNumber.getRandomNum(),
+        inputNumber.value,
+        randomNumber
+      );
     }
   });
 };
